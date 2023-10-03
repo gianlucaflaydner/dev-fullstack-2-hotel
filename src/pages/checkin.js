9aeb95a0440c8234d2c0463d71195fe75da399f0
Lucas Kremer <lucaskremerw@gmail.com>
2023-10-02 22:58:28 -0300

    feat: create all project routes

---
+++ src/pages/checkin.js
@@ -0,0 +1,9 @@
+export default function Checkin(){
+
+    return(
+
+        <div className="w-screen min-h-screen flex flex-col items-center justify-center bg-[#2f2c2c]">
+         
+        </div>
+    )
+}
