abe30d4e924b18db82198cf001570298b0fe4d66
Pedro <pedrosprandel27@gmail.com>
2023-10-11 00:23:27 -0300

    fix: unused-variable

--- src/pages/api/checkin.js
+++ src/pages/api/checkin.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
       };
       const novaReserva = await Reservas.create(reserva);
 
-      const quartoAtualizado = await Quartos.findByIdAndUpdate(
+      await Quartos.findByIdAndUpdate(
         parsedBody.quarto,
         { $push: { reservas: novaReserva._id } },
         { new: true }
