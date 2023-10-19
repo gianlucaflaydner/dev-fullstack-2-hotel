15730d8984495443b8f89e72510ddb204739451b
Pedro <pedrosprandel27@gmail.com>
2023-10-19 16:12:16 -0300

    feat: checkin

--- src/pages/api/checkin.js
+++ src/pages/api/checkin.js
@@ -1,31 +1,30 @@
 import Hospede from "@/models/Hospedes";
 import connectDatabase from "@/services/database";
-import _ from "lodash";
+import Reserva from "../reserva";
 
 export default async function handler(req, res) {
-  const { query, method } = req;
+  const { method } = req;
 
   await connectDatabase();
 
   if (method === "GET") {
     try {
-      const hospede = await Hospede.findOne({ cpf: query.cpf });
-      console.log("hospede", hospede);
-      res.status(200).json({ success: true });
-    } catch (error) {
-      res.status(500).json({ success: false, errors: error });
-    }
-  } else {
-    try {
       const { query } = req;
       const { cpf } = query;
 
       const hospede = await Hospede.findOne({ cpf });
-      if (!_.isEmpty(hospede?.reserva)) {
-        res
-          .status(500)
-          .json({ success: false, errors: "Nenhuma reserva neste CPF" });
-      }
+      const reservas = await Reserva.find({});
+
+      const reservaRespectiva = reservas.find(
+        (reserva) => reserva.hospede === hospede._id
+      );
+
+      const response =
+        reservaRespectiva === undefined
+          ? { success: false, errors: "Nenhuma reserva neste CPF" }
+          : { success: true };
+
+      res.status(500).json(response);
     } catch (error) {
       res.status(500).json({ success: false, errors: error });
     }
